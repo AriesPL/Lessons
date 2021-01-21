a6c4dcc8a4037fdff2b0e054bd3ad9cb97b57768
AriesPL <blackhorns1989@gmail.com>
2021-01-21 18:49:54 +0300

    Class Point

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,6 +10,16 @@ namespace Snake
 	{
 		static void Main(string[] args)
 		{
+			Point p1 = new Point();
+			p1.x = 5;
+			p1.y = 10;
+			p1.sym = '%';
+
+			p1.Drow(p1.x,p1.y,p1.sym);
+
+			Console.ReadLine();
 		}
+
+		
 	}
 }
