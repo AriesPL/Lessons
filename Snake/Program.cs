17df542f82284e3377515fad5b9300ec7c5d5278
AriesPL <blackhorns1989@gmail.com>
2021-01-25 10:12:40 +0300

    add Horizontal and vertical Line

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,12 +10,20 @@ namespace Snake
 	{
 		static void Main(string[] args)
 		{
+			
+
 			Point p1 = new Point(1,2,'%');
-			//p1.x = 5;
-			//p1.y = 10;
-			//p1.sym = '%';
 
 			p1.Drow();
+
+			HorizontalLine horLineUp = new HorizontalLine(0,50,0,'+');
+			HorizontalLine horLineDown = new HorizontalLine(0, 50, 25, '+');
+			VerticalLine verticalLineLeft = new VerticalLine(0, 0, 25, '|');
+			VerticalLine verticalLineRight = new VerticalLine(50, 0, 25, '|');
+			horLineUp.Drow();
+			horLineDown.Drow();
+			verticalLineLeft.Drow();
+			verticalLineRight.Drow();
 
 			Console.ReadLine();
 		}
