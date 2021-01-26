09bf36cdb39b8997fc770180479c9e27a5d14aec
AriesPL <blackhorns1989@gmail.com>
2021-01-26 10:12:12 +0300

    Class Figura

---
+++ Snake/Figura.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+	class Figura
+	{
+		//Класс фигура со списком к которому имеют доступ только наследники 
+		//и методом отрисовкой полей
+		protected List<Point> lPoint;
+
+		public void Drow()
+		{
+			foreach (Point p in lPoint)
+			{
+				p.Drow();
+			}
+
+		}
+	}
+}
