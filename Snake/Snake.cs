73b4deeaeadd5f7148aa49ff0597f83af03a7eae
AriesPL <blackhorns1989@gmail.com>
2021-01-28 10:30:24 +0300

    Hidden realisation move snake

--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -8,7 +8,7 @@ namespace Snake
 {
 	class Snake : Figura
 	{
-		public Direction direction;
+		 Direction direction;
 		public Snake(Point tail, int lenght, Direction _direction)
 		{
 			direction = _direction;
@@ -42,6 +42,18 @@ namespace Snake
 			Point nextPoint = new Point(head);
 			nextPoint.Move(1, direction);
 			return nextPoint;
+		}
+
+		public void HandKey(ConsoleKey key)
+		{
+			if (key == ConsoleKey.RightArrow)
+				direction = Direction.RIGHT;
+			else if (key == ConsoleKey.LeftArrow)
+				direction = Direction.LEFT;
+			else if (key == ConsoleKey.DownArrow)
+				direction = Direction.DOWN;
+			else if (key == ConsoleKey.UpArrow)
+				direction = Direction.UP;
 		}
 	}
 }
