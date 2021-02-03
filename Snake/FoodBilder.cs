cb5f5ec5c4f46517ccfecc2442eed7a2fd9aa683
AriesPL <blackhorns1989@gmail.com>
2021-02-03 20:32:30 +0300

    Class FoodBilder

---
+++ Snake/FoodBilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+	class FoodBilder
+	{
+		//Класс по созданию еды для змейки. Конструктор который пнимает пораметры и метод выбора рандомной позиции для еды.
+		int wightWindow;
+		int hightWindow;
+		char food;
+
+		Random rnd = new Random();
+
+		public FoodBilder(int wightWindow, int hightWindow, char food)
+		{
+			this.wightWindow = wightWindow;
+			this.hightWindow = hightWindow;
+			this.food = food;
+		}
+
+		public Point GiveMeFood()
+		{
+			int x = rnd.Next(2, wightWindow - 2);
+			int y = rnd.Next(2, hightWindow - 2);
+			return new Point(x, y, food);
+		}
+
+	}
+}
