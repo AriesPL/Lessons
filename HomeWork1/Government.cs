df10748b4f8ebe64c264f751a96fbf83249d0e2f
AriesPL <blackhorns1989@gmail.com>
2021-02-25 09:06:41 +0300

    task 6

---
+++ HomeWork1/Government.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+	 class Government
+	{
+		public Government()
+		{
+
+		}
+		public void Pouse() 
+		{
+			Console.Read();
+		}
+
+		public void Print()
+		{
+			Console.WriteLine();
+		}
+	}
+}
